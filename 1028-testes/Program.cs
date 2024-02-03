cd92e709d936b37e507dced9f88e0f9aefc53c91
Eduardo Junior <51997209+ejunior27@users.noreply.github.com>
2024-02-02 21:29:42 -0300

    init

---
+++ 1028-testes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1028_testes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
